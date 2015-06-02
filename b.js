2e0f97b5b71abd2b0007f0aec66a279931c8ad14
leffen <leffen@rpi-1.(none)>
2015-06-02 22:51:10 +0000

    Adding bluetooh

--- b.js
+++ b.js
@@ -1,9 +1,14 @@
 var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+var car1='30:14:06:09:10:16';
+var mac='00:1F:5B:E1:F4:4D';
+
 
 btSerial.on('found', function(address, name) {
     btSerial.findSerialPortChannel(address, function(channel) {
-        console.log(address);
-        btSerial.connect(address, channel, function() {
+        console.log(address,channel,name);
+        if(address===mac) {
+          console.log('trying to connect to ' + name); 
+          btSerial.connect(address, channel, function() {
             console.log('connected');
 
             btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
@@ -19,6 +24,7 @@ btSerial.on('found', function(address, name) {
 
         // close the connection when you're ready
         btSerial.close();
+        }
     }, function() {
         console.log('found nothing');
     });
