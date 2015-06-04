d205e58710f84dd69d0323a7b58ebca6e94dc4ce
leffen <leffen@gmail.com>
2015-06-05 01:30:16 +0200

    WIP

--- b.js
+++ b.js
@@ -1,33 +1,56 @@
 var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-var car1='30:14:06:09:10:16';
-var mac='00:1F:5B:E1:F4:4D';
+var readlineSync = require('readline-sync');
 
 
-btSerial.on('found', function(address, name) {
-    btSerial.findSerialPortChannel(address, function(channel) {
-        console.log(address,channel,name);
-        if(address===mac) {
-          console.log('trying to connect to ' + name); 
-          btSerial.connect(address, channel, function() {
-            console.log('connected');
+var car1 = '30-14-06-09-10-16';
 
-            btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
-                if (err) console.log(err);
-            });
+function getCommand() {
+  return readlineSync.question('Command :');
+}
 
-            btSerial.on('data', function(buffer) {
-                console.log(buffer.toString('utf-8'));
-            });
-        }, function () {
-            console.log('cannot connect');
+function listen(btSerial) {
+  var finished = false;
+
+  while (!finished) {
+
+    cmd = getCommand();
+    if (cmd === 'q') {
+      finished = true;
+    } else {
+      btSerial.write(new Buffer('r', 'utf-8'), function (err, bytesWritten) {
+        if (err) console.log(err);
+      });
+    }
+  }
+  btSerial.close();
+}
+
+
+btSerial.on('found', function (address, name) {
+  console.log('Checking for channels ', address, name);
+  btSerial.findSerialPortChannel(address, function (channel) {
+    console.log('found  ' + address);
+    if (address === car1) {
+      console.log('trying to connect to ' + name);
+
+      btSerial.connect(address, channel, function () {
+        console.log('Connected to ' + address);
+
+        btSerial.on('data', function (buffer) {
+          console.log(buffer.toString('utf-8'));
         });
 
-        // close the connection when you're ready
-        btSerial.close();
-        }
-    }, function() {
-        console.log('found nothing');
-    });
+        listen(btSerial);
+      }, function () {
+        console.log('cannot connect');
+      });
+
+      // close the connection when you're ready
+      btSerial.close();
+    }
+  }, function () {
+    console.log('found nothing');
+  });
 });
 
 btSerial.inquire();
